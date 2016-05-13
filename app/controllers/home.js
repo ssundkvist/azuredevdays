274ea09036310382563a0f4eebf81b578743f52c
Stefan Sundkvist <sst@kth.se>
2016-05-13 11:38:41 +0200

    github test

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
 router.get('/', function (req, res, next) {
   var articles = [new Article(), new Article()];
     res.render('index', {
-      title: 'Don Ove copy',
+      title: 'Hej Hopp!',
       articles: articles
     });
 });
